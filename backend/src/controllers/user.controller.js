627e7dddfe954f1218b385f30fc221864ae943f2
Taranpreet Singh <taranpreet1911@gmail.com>
2026-04-16 15:22:17 +0530

    made the login and logout contoller functions and authentication middleware

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -150,7 +150,11 @@ const loginUser = asyncHandler(async (req, res) => {
         secure: true
     }
 
-    return res.status(200).cookie("accessToken", options).cookie("refreshToken", options).json(
+    return res
+    .status(200)
+    .cookie("accessToken", options)
+    .cookie("refreshToken", options)
+    .json(
         new ApiResponse(
             200,
             {
@@ -162,4 +166,33 @@ const loginUser = asyncHandler(async (req, res) => {
 
 })
 
-export { registerUser }
+const logoutUser = asyncHandler(async(req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged Out"))
+})
+
+export { 
+    registerUser,
+    loginUser,
+    logoutUser
+ }
